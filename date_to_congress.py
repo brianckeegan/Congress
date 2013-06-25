cd33556ce162062370ba79caf19bf03dbae11021
Brian Keegan <bkeegan@gmail.com>
2013-06-25 19:26:10 -0300

    Create date_to_congress.py

---
+++ date_to_congress.py
@@ -0,0 +1,17 @@
+import datetime
+
+def convert_date_to_congress(date):
+    first = [datetime.date(i,3,4) for i in range(1789,1935,2)]
+    second = [datetime.date(i,1,3) for i in range(1935,datetime.date.today().year+3,2)]
+    start_dates = {i:num+1 for num,i in enumerate(first+second)}
+    start_date_list = sorted(start_dates.keys())
+    if date < datetime.date(1789,3,4):
+        raise ValueError("Pre-dates first U.S. Congress")
+    elif date > datetime.date.today():
+        raise ValueError("Date is in the future")
+    else:
+        for num,start_date in enumerate(start_date_list[:-1]):
+            if start_date <= date < start_date_list[num+1]:
+                congress_number = start_dates[start_date]
+            
+    return congress_number
