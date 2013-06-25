6db511dd3de396e72b18f7cd79eb8056f170c81c
Brian Keegan <bkeegan@gmail.com>
2013-06-25 19:37:10 -0300

    Update date_to_congress.py

--- date_to_congress.py
+++ date_to_congress.py
@@ -1,6 +1,10 @@
 import datetime
 
 def convert_date_to_congress(date):
+    '''
+    Takes a datetime.date object and returns an integer corresponding to the Congress
+    in session on that date. Based on: http://en.wikipedia.org/wiki/List_of_United_States_Congresses
+    '''
     first = [datetime.date(i,3,4) for i in range(1789,1935,2)]
     second = [datetime.date(i,1,3) for i in range(1935,datetime.date.today().year+3,2)]
     start_dates = {i:num+1 for num,i in enumerate(first+second)}
